d11da1de6b788b038ca51716b141b23ca672ee43
Olya test <ushakovaolga1989@gmail.com>
2023-07-12 23:28:13 +0300

    ParameterizedTest added

--- src/test/java/ru/netology/services/countmvn/service/WorkAndRestServiceTest.java
+++ src/test/java/ru/netology/services/countmvn/service/WorkAndRestServiceTest.java
@@ -2,29 +2,31 @@ package ru.netology.services.countmvn.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WorkAndRestServiceTest {
-    @Test
-    public void shouldCalculateForWork() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000,3000,20000,3",
+            "100000,60000,150000,2"
+    })
+    public void shouldCalculateForWork(int income, int expenses, int threshold, int expected) {
         WorkAndRestService service = new WorkAndRestService();
 
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldCalculateForRest() {
-        WorkAndRestService service = new WorkAndRestService();
-
-        int income = 100000;
-        int expenses = 60000;
-        int threshold = 150000;
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void shouldCalculateForRest() {
+//        WorkAndRestService service = new WorkAndRestService();
+//
+//        int income = 100000;
+//        int expenses = 60000;
+//        int threshold = 150000;
+//        int expected = 2;
+//        int actual = service.calculate(income, expenses, threshold);
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
